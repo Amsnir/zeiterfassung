fe33700279296dcd1bff4d3128e8b2eb1f400b5c
Janis0001 <janis.papai@htl-saalfelden.at>
2024-01-16 11:34:21 +0100

    First_Try

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,92 +1,164 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Homepage(),
+    );
+  }
+}
+
 class Homepage extends StatefulWidget {
   const Homepage({Key? key}) : super(key: key);
-
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<Homepage> {
-  final TextEditingController _timeController = TextEditingController();
-
-  DateTime now = DateTime.now();
-  late final String _formatted = DateFormat('hh:mm').format(now);
   List<String> dropdownItems = ['Normale Buchung', 'Dienstgang', 'Homeoffice'];
   String selectedDropdownItem = 'Normale Buchung';
   bool isChecked = false;
+  late Timer _timer;
+  late String _formatted;
 
+  @override
+  void initState() {
+    super.initState();
 
-  void buchen(){
-    //final newBuchung = Buchungen(BU_ID: 3000, BU_DN_ID: 22, BU_TIMESTAMP: DateTime.now(), BU_ABW_NR: 1);
-    //Hive.box<Buchungen>('buchungen').add(newBuchung);
+    _formatted = DateFormat('HH:mm').format(DateTime.now());
+
+    _timer = Timer.periodic(Duration(seconds: 1), (Timer timer) {
+      setState(() {
+        _formatted = DateFormat('HH:mm').format(DateTime.now());
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    // Cancel the timer to prevent memory leaks
+    _timer.cancel();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(height: 180.0),
-              // Display current time
-              Text(
-                _formatted,
-                textAlign: TextAlign.center,
-                style: const TextStyle(fontSize: 50.0),
+      body: Column(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(20.0),
+                bottomRight: Radius.circular(20.0),
               ),
-              const SizedBox(height: 20.0), // Add some spacing
-              // Checkbox and text
-              Row(
+            ),
+            child: Image.asset(
+              'assets/LHR_Logo_orange-blau_RGB_transparent_Latzer.png',
+              width: 300.0,
+              height: 200.0,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Checkbox(
-                    value: isChecked,
-                    onChanged: (bool? value) {
-                      setState(() {
-                        isChecked = value!;
-                      });
-                    },
+                  SizedBox(height: 20.0),
+                  Text(
+                    _formatted,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 50.00, color: Color(0xFF443B5A), fontWeight: FontWeight.bold),
                   ),
-                  const SizedBox(width: 8.0),
-                  const Text(
-                    'Buchungsende',
-                    style: TextStyle(fontSize: 18.0),
+                  SizedBox(height: 20.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Checkbox(
+                        value: isChecked,
+                        onChanged: (bool? value) {
+                          setState(() {
+                            isChecked = value!;
+                          });
+                        },
+                      ),
+                      const SizedBox(width: 8.0),
+                      Text(
+                        'Buchungsende',
+                        style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20.0),
+                  if (isChecked == false)
+                    Center(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20.0),
+                          color: Colors.grey[300],
+                        ),
+                        child: DropdownButton<String>(
+                          value: selectedDropdownItem,
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              selectedDropdownItem = newValue!;
+                            });
+                          },
+                          items: dropdownItems.map<DropdownMenuItem<String>>(
+                                (String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Container(
+                                  padding: EdgeInsets.all(10.0),
+                                  child: Text(
+                                    value,
+                                    style: TextStyle(
+                                      color: Colors.grey[800],
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              );
+                            },
+                          ).toList(),
+                          dropdownColor: Colors.grey[300],
+                        ),
+                      ),
+                    ),
+                  Spacer(),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Implement booking logic here
+                    },
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: Size(200, 50),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      primary: Color(0xFF443B5A),
+                    ),
+                    child: Text(
+                      'Buchen',
+                      style: TextStyle(fontSize: 20.0, color: Colors.white),
+                    ),
                   ),
                 ],
               ),
-              const SizedBox(height: 20.0),
-              // Dropdown menu
-              if (isChecked==false)
-                Center(
-                  child: DropdownButton<String>(
-                    value: selectedDropdownItem,
-                    onChanged: (String? newValue) {
-                      setState(() {
-                        selectedDropdownItem = newValue!;
-                      });
-                    },
-                    items: dropdownItems.map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
-                  ),
-                ),
-              const Spacer(),
-              // "Buchen" Button
-              ElevatedButton(
-                onPressed: buchen,
-                child: const Text('Buchen'),
-              ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
