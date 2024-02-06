2008fc31c4cc1b1eb2dc5b2336dfe29c5a4afb66
Amsnir <99396044+Amsnir@users.noreply.github.com>
2024-02-06 12:04:58 +0100

    Get/Post requests

--- lib/api/apiHandler.dart
+++ lib/api/apiHandler.dart
@@ -1,6 +1,25 @@
+import 'dart:convert';
+import 'dart:math';
+
 import 'package:http/http.dart' as http;
 
-Future<http.Response> fetchAlbum() {
-  return http.get(Uri.parse(
-      'https://app.lohn.at/Self/api/v1/firmengruppen/GF/firmen/1/dienstnehmer/6669'));
+String _bearerToken = "";
+String _urlPOST =
+    "https://app.lohn.at/Self/login?username=NORRISCHU6669&password=cQkAEEWH§";
+String _urlGET =
+    "https://app.lohn.at/Self/api/v1/firmengruppen/GF/firmen/1/dienstnehmer/6669";
+
+Future<http.Response> fetchDienstnehmer() async {
+  return await http.get(
+    Uri.parse(_urlGET),
+    headers: {
+      'Authorization': 'Bearer $_bearerToken',
+    },
+  );
+}
+
+Future<void> fetchBearerToken() async {
+  final response = await http.post(Uri.parse(_urlPOST));
+
+  print(response.body.toString());
 }
--- lib/api/synchdb.dart
+++ lib/api/synchdb.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:http/http.dart';
 import 'package:zeiterfassung_v1/api/apiHandler.dart';
 import 'package:zeiterfassung_v1/hivedb/hivedb_Klassen/dienstnehmer.dart';
 import 'package:zeiterfassung_v1/hivedb/hivefactory.dart';
@@ -8,8 +9,9 @@ import 'package:zeiterfassung_v1/hivedb/hivefactory.dart';
 void synchData() async {
   final dienstnehmer = new Dienstnehmer();
 
-  //var data = fetchAlbum();
-  // log(data.toString());
+  var data2 = await fetchBearerToken();
+  var data = await fetchDienstnehmer();
+  log(data.toString());
   dienstnehmer.DN_ID = 1;
   dienstnehmer.DN_FA_KZ = 'd';
   dienstnehmer.DN_FA_NR = 1;
@@ -36,9 +38,9 @@ void synchData() async {
   // dienstnehmer3.DN_VORNAME = "Amir";
   // dienstnehmer3.DN_STATUS = 0;
 
-  final dienstnehmerbox =
-      await HiveFactory.openBox<Dienstnehmer>('dienstnehmer');
-  dienstnehmerbox.add(dienstnehmer);
+  // final dienstnehmerbox =
+  //     await HiveFactory.openBox<Dienstnehmer>('dienstnehmer');
+  // dienstnehmerbox.add(dienstnehmer);
   // dienstnehmerbox.add(dienstnehmer2);
   // dienstnehmerbox.add(dienstnehmer3);
 }
